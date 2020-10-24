329f947deb3982a2685ba0c8c9bdeec7cf727d18
Brian <briandsalemi@gmail.com>
2020-10-24 09:05:44 -0400

    Added state to App.js and created a constants.js file for BASE_URL and EXCHANGE_URL variables

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from 'react';
 
 
 class App extends React.Component{
+  
+  state = {
+    currencyNames: [],
+    convertedAmount: null,
+    returnRate: null
+  }
+
   render(){
     return
   }
---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const BASE_URL = "https://api.exchangeratesapi.io/latest"
+export const EXCHANGE_URL = "https://api.exchangeratesapi.io/latest?base="
