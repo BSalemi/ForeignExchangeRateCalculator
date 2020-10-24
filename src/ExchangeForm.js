c945eafdac50a47231743e80f46c7c845a3b20ab
Brian <briandsalemi@gmail.com>
2020-10-24 09:45:47 -0400

    Created the setCurrencies, updateAmount, and calculateExchangeRate functions to the ExchangeForm Component

--- src/ExchangeForm.js
+++ src/ExchangeForm.js
@@ -17,6 +17,38 @@ class ExchangeForm extends React.Component {
         return currencyOptions
     }
 
+    setCurrencies = (e) => {
+        this.setState({
+            ...this.state,
+            [e.target.parentElement.name]: e.target.value
+        })
+    }
+
+    updateAmount = (e) => {
+        this.setState({
+            ...this.state,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    calculateExchangeRate = (e) => {
+        e.preventDefault()
+        const {baseCurrency, returnCurrency, amount} = this.state,
+              {setConvertedAmount} = this.props
+
+        if(!baseCurrency || !returnCurrency || !amount){
+            alert("Please select both a base and return currency and enter an amount.")
+        }  else {
+            fetch(EXCHANGE_URL + baseCurrency)
+            .then(res => res.json())
+            .then(data => {
+                let converted = (amount * data.rates[returnCurrency]),
+                    num = converted.toFixed(2)
+                setConvertedAmount(num, returnCurrency)
+            })
+        }
+    }
+
     render(){
         const {currencyNames} = this.props
 
@@ -27,13 +59,15 @@ class ExchangeForm extends React.Component {
                     {this.generateCurrencyOptions(currencyNames)}
                 </select>
                 <label>Amount:</label>
-                <input placeholder="Amount" type="number" min="0" step="any" name="amount" value={this.state.amount}/>
+                <input placeholder="Amount" type="number" min="0" step="any" name="amount" value={this.state.amount}  onChange={e => this.updateAmount(e)}/>
                 <label>Convert To:</label>
                 <select name="returnCurrency">
                     {this.generateCurrencyOptions(currencyNames)}
                 </select>
                 <div className="containerDiv">
-                    <button className="convertButton" >Convert Amount</button>
+                    <button className="convertButton" onClick={e => this.calculateExchangeRate(e)} >
+                        Convert Amount
+                    </button>
                 </div>
             </form>
         )
